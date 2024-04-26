f6b2215b39d6862edfb56d9f16d830ef420f4237
joao_cc <joao_cc@yahoo.com>
2024-04-26 16:36:54 +0200

    deployment part2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['qpv8w1c3-8000.euw.devtunnels.ms', 'localhost', '127.0.0.1', 'boutique-adoci-e8a7ada89483.herokuapp.com/']
+ALLOWED_HOSTS = ['qpv8w1c3-8000.euw.devtunnels.ms', 'localhost', '127.0.0.1', 'boutique-adoci-e8a7ada89483.herokuapp.com']
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
